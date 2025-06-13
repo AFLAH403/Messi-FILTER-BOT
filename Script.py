59690bfea496b30027b33bc24afffa2ca39890d7
Muhammed Aflah <aflahtbm@gmail.com>
2025-06-13 08:29:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -246,7 +246,7 @@ Loki S01 or Loki S01E04 or Lucifer S03E24</pre>
     CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
 ╔═══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╗ 
-➲ <a href=https://t.me/MrTechMovies_Group> @CINEMA_COMPANY </a>
+➲ <a href=https://t.me/CINEMA_COMPANYE_Backup> @CINEMA_COMPANY </a>
 ➲ <a href=https://t.me/TBM_Bot_Updates> @TBM_Bot_Updates </a>
 ╚═══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╝</b>"""
 
