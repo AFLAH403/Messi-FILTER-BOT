c0a2da2b3a26baf6f842b626918f56c688f6c0c5
AFLAH403 <githubtbm@gmail.com>
2025-06-18 08:12:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7784630528:AAFsruxUoLdJj3vx_Pvp1Ta0QtIhtBi
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://telegra.ph/file/88f817d27a81434857f05-e047f5edacea6fd216.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/116a1f5b2b6fc34ab9831-3310bc4ddb1f239a0f.jpg')).split()
 
 
 # Admins & Users
