ce684fd87469ba4e630196f0c3c0ea127ef4e942
AFLAH403 <githubtbm@gmail.com>
2025-07-19 06:36:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -460,7 +460,8 @@ async def start(client, message):
                 reply_markup=reply_markup
             )
             filesarr.append(msg)
-        k = await client.send_message(chat_id 
+           await client.send_message(chat_id, "Your message")
+    
         return     elif data.startswith("files"):
         user = message.from_user.id
         if temp.SHORT.get(user)==None:
